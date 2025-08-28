95de9a00d214e679710e1f90973bd8e92a7117bd
Mengchun <c3053055@newcastle.ac.uk>
2025-08-28 12:58:38 +0100

    Delete R directory

--- R/imports.R
+++
@@ -1,9 +0,0 @@
-#' @importFrom stats var sd binomial density median update
-#' @importFrom rjags jags.model coda.samples
-#' @importFrom coda effectiveSize gelman.diag
-#' @importFrom parallel detectCores mclapply makeCluster clusterExport clusterEvalQ parLapply stopCluster
-#' @importFrom matrixStats rowVars
-#' @importFrom limma makeContrasts
-#' @importFrom graphics hist par text lines
-
-NULL
